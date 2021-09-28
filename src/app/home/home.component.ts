372774272941b5e763e274ef3f2899d9a24258c4
maldonadojudith18 <judith1821maldonado@gmail.com>
2021-09-28 14:19:22 -0500

    interpolation #5

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,12 +5,23 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-
 export class HomeComponent implements OnInit {
+
+  itemCount: number = 0;
+  btnText: string = 'Add an item';
+  goalText: string = 'My first life goal';
+  goals : any[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.itemCount = this.goals.length;
+  }
+
+  addItem(){
+    this.goals.push(this.goalText);
+    this.goalText = '';
+    this.itemCount = this.goals.length;
   }
 
 }
